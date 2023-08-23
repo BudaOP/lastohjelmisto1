3ad6cc75b5862b4d687145a3546d59ad0751fba0
BudaOP <ibudanov24@gmail.com>
2023-08-23 18:01:02 +0300

    yuo

--- TESTI.py
+++ TESTI.py
@@ -1,3 +1,23 @@
 print("hei maailma")
 print('moro')
 print('testi3 to update')
+print('Hyvää\npäivää')
+
+# user = input('your username: ')
+# print('Nice to meet you, '+ user + '!')
+
+# fahrenheit_str = input("Anna lämpötila Fahrenheit-asteina: ")
+# fahrenheit = float(fahrenheit_str)
+# celsius = (fahrenheit-32)*5/9
+# print("Lämpötila Celsius-asteina: " + str(celsius))
+
+import math
+
+print(f"{'Pii':12s}:{math.pi:10.5f}")
+print(f"{'Neperin luku':12s}:{math.e:10.5f}")
+
+# ikä = int(input("Anna ikä: "))
+# if 15<=ikä<18:
+#     paino = float(input("Anna paino (kg): "))
+# if (ikä>=18 or ikä>=15 and paino>=55):
+#     print("Lääkkeen käyttö on sallittua.")
---
+++ moduuli1.py
@@ -0,0 +1,81 @@
+# # 1. tehtävä
+# userName = input('Kirjoita nimesi: ')
+# print('Hei, ' + userName + '!')
+#
+# # 2. tehtävä
+# import math
+#
+# säde_str = input('Kirjoita ympyrän säde (metri): ')
+# säde = int(säde_str)
+# ympyränA = säde**2 * math.pi
+# print('Ympyrän pinta-ala on ', round(ympyränA), 'metriä')
+#
+# # 3. tehtävä
+# kanta = int(input("Anna suorakulmion kannan pituus : "))
+# korkeus = int(input("Anna suorakulmion korkeus : "))
+#
+# piiri = kanta * 2 + korkeus * 2
+# pintaAla = kanta * korkeus
+#
+# print('Suorakulmion piiri on ', piiri, 'ja pinta-ala on', pintaAla)
+
+# 4. tehtävä
+# a = int(input('Kirjoita ensimmäinen luku: '))
+# b = int(input('Kirjoita toinen luku: '))
+# c = int(input('Kirjoita kolmas luku: '))
+#
+# summa = a + b + c
+# tulo = a * b * c
+# keskiarvo = (a + b + c) / 3
+#
+# print('Lukujen summa on', summa, '\nLukujen tulo on', tulo, '\nLukujen keskiarvo on', int(keskiarvo))
+#
+# # 5. tehtävä
+# leiviskät_str = input('Anna leiviskät: ')
+# naulat_str = input('Anna naulat: ')
+# luodit_str = input('Anna luodit: ')
+#
+# leiviskät = float(leiviskät_str)
+# naulat = float(naulat_str)
+# luodit = float(luodit_str)
+#
+# luotiCalc = (leiviskät * 20 + naulat) * 32 + luodit
+# massaCalc = (luotiCalc * 13.3) / 1000
+#
+# kg = int(massaCalc)
+# g = round((massaCalc - int(massaCalc)) * 1000, 2)
+#
+# print('Massa nykymittojen mukaan:\n', kg, 'kilogrammaa ja', g, 'grammaa')
+#
+# # 6. tehtävä
+import random
+
+# 3digit
+
+a = str(random.randint(0, 9))
+b = str(random.randint(0, 9))
+c = str(random.randint(0, 9))
+
+
+numerokoodi1 = a+b+c
+
+print(numerokoodi1)
+
+# 4digit
+
+d = str(random.randint(1, 6))
+e = str(random.randint(1, 6))
+f = str(random.randint(1, 6))
+g = str(random.randint(1, 6))
+
+numerokoodi2 = d+e+f+g
+print(numerokoodi2)
+
+
+#toinen tapa 4digit
+numerot = random.sample(range(1, 6), 4)
+
+numerokoodi3 = str(numerot[0]) + str(numerot[1]) + str(numerot[2]) + str(numerot[3])
+
+print(numerokoodi3)
+
