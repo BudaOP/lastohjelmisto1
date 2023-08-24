ea855740550e813680ecf7a3b171f6b7e293e1fe
BudaOP <ibudanov24@gmail.com>
2023-08-24 19:08:16 +0300

    yuo

--- TESTI.py
+++ TESTI.py
@@ -1,30 +1,37 @@
-print("hei maailma")
-print('moro')
-print('testi3 to update')
-print('Hyvää\npäivää')
-
-# user = input('your username: ')
-# print('Nice to meet you, '+ user + '!')
-
-# fahrenheit_str = input("Anna lämpötila Fahrenheit-asteina: ")
-# fahrenheit = float(fahrenheit_str)
-# celsius = (fahrenheit-32)*5/9
-# print("Lämpötila Celsius-asteina: " + str(celsius))
-
-import math
-
-print(f"{'Pii':12s}:{math.pi:10.5f}")
-print(f"{'Neperin luku':12s}:{math.e:10.5f}")
-
-# ikä = int(input("Anna ikä: "))
-# if 15<=ikä<18:
-#     paino = float(input("Anna paino (kg): "))
-# if (ikä>=18 or ikä>=15 and paino>=55):
-#     print("Lääkkeen käyttö on sallittua.")
-
-
-kerrat = int(input("Montako kertaa tervehditään: "))
-tehdyt = 0
-while tehdyt<kerrat:
-    print("Hyvää huomenta")
-    tehdyt = tehdyt + 1
+# print("hei maailma")
+# print('moro')
+# print('testi3 to update')
+# print('Hyvää\npäivää')
+#
+# # user = input('your username: ')
+# # print('Nice to meet you, '+ user + '!')
+#
+# # fahrenheit_str = input("Anna lämpötila Fahrenheit-asteina: ")
+# # fahrenheit = float(fahrenheit_str)
+# # celsius = (fahrenheit-32)*5/9
+# # print("Lämpötila Celsius-asteina: " + str(celsius))
+#
+# import math
+#
+# print(f"{'Pii':12s}:{math.pi:10.5f}")
+# print(f"{'Neperin luku':12s}:{math.e:10.5f}")
+#
+# # ikä = int(input("Anna ikä: "))
+# # if 15<=ikä<18:
+# #     paino = float(input("Anna paino (kg): "))
+# # if (ikä>=18 or ikä>=15 and paino>=55):
+# #     print("Lääkkeen käyttö on sallittua.")
+#
+#
+# kerrat = int(input("Montako kertaa tervehditään: "))
+# tehdyt = 0
+# while tehdyt<kerrat:
+#     print("Hyvää huomenta")
+#     tehdyt = tehdyt + 1
+#
+#     komento = input("Anna komento: ")
+#     while komento != "lopeta":
+#         print("Suoritan toiminnon: " + komento)
+#         komento = input("Anna komento: ")
+#     print("Toiminnot lopetettu.")
+#
--- moduuli 4.py
+++ moduuli 4.py
@@ -1,1 +1,16 @@
-# tästä alkaa moduuli 4
+# # 1. tehtävä
+# luku = 0
+#
+# while luku < 1000:
+#     luku = luku + 1
+#     lukuCalc = luku / 3
+#     tulos = lukuCalc == int(lukuCalc)
+#     if tulos:
+#         print(luku)
+
+# 2. tehtävä
+# tuuma = float(input('Montako tuumaa: '))
+#
+# while tuuma >= 0:
+#     print(f'{tuuma * 2.54} cm')
+#     break
