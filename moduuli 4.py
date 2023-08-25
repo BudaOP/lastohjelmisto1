4409bbc2af8eaeab77700a3e796dcb07ea09e03e
BudaOP <ibudanov24@gmail.com>
2023-08-25 11:15:07 +0300

    yuo

--- moduuli 4.py
+++ moduuli 4.py
@@ -7,6 +7,7 @@
 #     tulos = lukuCalc == int(lukuCalc)
 #     if tulos:
 #         print(luku)
+import random
 
 # 2. tehtävä
 # tuuma = float(input('Montako tuumaa: '))
@@ -14,3 +15,63 @@
 # while tuuma >= 0:
 #     print(f'{tuuma * 2.54} cm')
 #     break
+
+# 3. tehtävä
+# luku = input('Kirjoita luku: ')
+#
+# if luku != '':
+#     max = int(luku)
+#     min = int(luku)
+#
+#     while luku != '':
+#         luku1 = int(luku)
+#
+#         if luku1 > max:
+#             max = luku1
+#
+#         if luku1 < min:
+#             min = luku1
+#
+#         luku = input('Kirjoita seuraava luku: ')
+#     print('Suurin arvo:', max)
+#     print('Pienin arvo:', min)
+
+# 4. tehtävä
+# lukuX = random.randint(1,10)
+# lukuInput = int(input('Arvaa luku: '))
+#
+# while lukuInput != lukuX:
+#     if lukuInput > lukuX:
+#         print('Liian suuri arvaus')
+#     elif lukuInput < lukuX:
+#         print('Liian pieni arvaus')
+#     lukuInput = int(input('Arvaa luku uudestaan: '))
+# print('Oikein')
+
+# 5. tehtävä
+# user_input = input('Kirjoita käyttäjätunnus: ')
+# password_input = input('Kirjoita salasanasi: ')
+#
+# username = 'python'
+# password = 'rules'
+# väärin = 0
+#
+# if user_input == username and password_input == password:
+#     print('oikein')
+#
+#
+# while True:
+#     if user_input != username or password_input != password:
+#         väärin += 1
+#         print(f'Käyttäjätunnus tai salasana on väärin. Kokeile uudelleen. Yrityksiä jäljellä {5 - väärin + 1}.')
+#         user_input = input('Kirjoita käyttäjätunnus: ')
+#         password_input = input('Kirjoita salasanasi: ')
+#         if user_input == username and password_input == password:
+#             print('Oikein')
+#             break
+#         if väärin == 5:
+#             print('Pääsy evätty')
+#             break
+
+# 6. tehtävä
+# liian vaikee tehtävä tai liian huonosti selitettynä
